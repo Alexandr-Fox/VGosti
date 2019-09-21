f0b1bc854f005f1d4af8c96e5fb2d55b214d25dc
Alexandr-Fox <al.rostilismith@yandex.ru>
2019-09-21 21:38:37 +0500

    изменено:      node_modules/gh-pages/.cache/github.com!Alexandr-Fox!VGosti.git 	изменено:      src/panels/Home.js

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -8,7 +8,7 @@ const Home = ({ id, go, fetchedUser }) => (
 	<Panel id={id}>
 		<PanelHeader>ВГости: Ваши гости Вконтакте</PanelHeader>
 			<img className="Home" src={persik} alt="Persik The Cat"/>
-			<Button size="xs" level="2" onClick={go} data-to="persik">
+			<Button size="xl" level="2" onClick={go} data-to="persik">
 				Персик
 			</Button>
 	</Panel>
