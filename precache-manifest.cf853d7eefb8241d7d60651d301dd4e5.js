bb0c6322e111ccf9c38d3ace0398af4f15e22f7f
Alexandr-Fox <al.rostilismith@yandex.ru>
2019-09-21 18:27:23 +0500

    Updates

---
+++ precache-manifest.cf853d7eefb8241d7d60651d301dd4e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/VGosti/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "50a7c68a689a8c6f3577",
+    "url": "/VGosti/static/js/runtime~main.9f343678.js"
+  },
+  {
+    "revision": "10f2a47907e76f4f5e88",
+    "url": "/VGosti/static/js/main.ff2f3cdd.chunk.js"
+  },
+  {
+    "revision": "a72b8927cee339dc4562",
+    "url": "/VGosti/static/js/2.4446855a.chunk.js"
+  },
+  {
+    "revision": "10f2a47907e76f4f5e88",
+    "url": "/VGosti/static/css/main.6a34e39c.chunk.css"
+  },
+  {
+    "revision": "a72b8927cee339dc4562",
+    "url": "/VGosti/static/css/2.c91ad5c0.chunk.css"
+  },
+  {
+    "revision": "0b9fe29c670dd380110d84a8ec891bd8",
+    "url": "/VGosti/index.html"
+  }
+];
