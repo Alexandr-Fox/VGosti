09be2e8b3546ef9e889e9020c5e2a04505b670f3
Alexandr-Fox <al.rostilismith@yandex.ru>
2019-09-21 20:50:21 +0500

    изменено:      src/panels/Persik.js

--- src/panels/Persik.js
+++ src/panels/Persik.js
@@ -4,7 +4,7 @@ import {Panel, PanelHeader, HeaderButton, platform, IOS} from '@vkontakte/vkui';
 import persik from '../img/persik.png';
 import './Persik.css';
 import Icon28ChevronBack from '@vkontakte/icons/dist/28/chevron_back';
-import Icon24Back from '@vkontakte/icons/dist/24/browser_back';
+import Icon24Back from '@vkontakte/icons/dist/24/back';
 
 const osname = platform();
 
@@ -12,7 +12,7 @@ const Persik = props => (
 	<Panel id={props.id}>
 		<PanelHeader
 			left={<HeaderButton onClick={props.go} data-to="home">
-				{osname === IOS ? <Icon28ChevronBack/> : <Icon24BrowserBack/>}
+				{osname === IOS ? <Icon28ChevronBack/> : <Icon24Back/>}
 			</HeaderButton>}
 		>
 			Персик
