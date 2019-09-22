442b7b2bbdf850d784af6c1cd1945f3c0c0dab36
Alexandr-Fox <al.rostilismith@yandex.ru>
2019-09-22 13:40:08 +0500

    изменено:      node_modules/gh-pages/.cache/github.com!Alexandr-Fox!VGosti.git 	изменено:      src/panels/Home.js

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -2,13 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Panel, ListItem, Button, Group, Div, Avatar, PanelHeader, platform, IOS } from '@vkontakte/vkui';
 import persik from '../img/persik.png';
-import './Persik.css';
+import './Avatar.css';
 
-const Home = ({ id, go, fetchedUser }) => (
+const Home = ({ id, go }) => (
 	<Panel id={id}>
 		<PanelHeader>ВГости: Ваши гости ВКонтакте</PanelHeader>
-			<img className="Persik" src={persik} alt="Persik The Cat"/>
-			<Button size="xl" level="2" onClick={go} data-to="persik">
+			<img className="Avatar" src={persik} alt="Persik The Cat"/>
+			<Button size="l" level="1" onClick={go} data-to="persik">
 				Персик
 			</Button>
 	</Panel>
