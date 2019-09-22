3a2a74d0ae349e81c7b162eb78dbaf278cde5a3b
Alexandr-Fox <al.rostilismith@yandex.ru>
2019-09-22 13:42:17 +0500

    изменено:      node_modules/gh-pages/.cache/github.com!Alexandr-Fox!VGosti.git 	новый файл:    src/panels/Avatar.css

---
+++ build/precache-manifest.5b570a17936aae1301561f2c1091b1f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/VGosti/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "50a7c68a689a8c6f3577",
+    "url": "/VGosti/static/js/runtime~main.9f343678.js"
+  },
+  {
+    "revision": "67ad013692f159cc2ad9",
+    "url": "/VGosti/static/js/main.73732ccc.chunk.js"
+  },
+  {
+    "revision": "b205bb06d45081416ba2",
+    "url": "/VGosti/static/js/2.b66ec025.chunk.js"
+  },
+  {
+    "revision": "67ad013692f159cc2ad9",
+    "url": "/VGosti/static/css/main.a615c808.chunk.css"
+  },
+  {
+    "revision": "b205bb06d45081416ba2",
+    "url": "/VGosti/static/css/2.c91ad5c0.chunk.css"
+  },
+  {
+    "revision": "e911cc985a3fbe193a7be37e7ac53040",
+    "url": "/VGosti/index.html"
+  }
+];
